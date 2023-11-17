4449c3a4c840567c2a80bbeb618076ef3d95cd0b
Aristosdou <125508956+Aristosdou@users.noreply.github.com>
2023-11-16 21:42:16 -0500

    Update layout.py

--- app/ui/ui/components/layout.py
+++ app/ui/ui/components/layout.py
@@ -1,6 +1,14 @@
 import solara
-
+from .components.basket import basket, add_to_basket
 
 @solara.component
 def Layout(children=[]):
-    return solara.VBox(children=children)
+    basket_quantity = solara.reactive(0)
+
+    solara.watch(basket, lambda: basket_quantity.set(sum(item['quantity'] for item in basket.value)))
+
+    return solara.VBox(children=[
+        solara.Text(f"Basket Quantity: {basket_quantity.value}"),
+        *children,
+    ])
+
