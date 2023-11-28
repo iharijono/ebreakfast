9d0b5f8c45acd4dc38411f992aaa65a34ce84021
Aristotelis Dougalis <aristotelisdougalis@Aristoteliss-MacBook-Pro-3.local>
2023-11-28 13:29:46 -0500

    Added the counter first version

--- app/ui/ui/components/order.py
+++ app/ui/ui/components/order.py
@@ -3,16 +3,30 @@ import solara
 
 from ..data import menus
 
-
 @solara.component
 def MenuCard(name):
     menu = menus[name]
+
+    # Reactive variable to store the quantity
+    quantity = solara.reactive(0)
+
+    def increment():
+        quantity.value += 1
+
+    def decrement():
+        quantity.value -= 1
+
     with rv.Card(max_width="400px") as main:
         # with solara.Link(f"/menu/{name}"):
         rv.Img(height="250", src=menu.image_url)
         rv.CardTitle(children=[menu.price])
         with rv.CardText():
             solara.Markdown(menu.markdown)
+        with solara.Row(justify="space-around", margin=10):
+            solara.Button("add", color="green", icon_name="add", on_click=increment)
+            solara.Info(f"value: {quantity.value}")
+            solara.Button("min", color="green", icon_name="remove", on_click=decrement)
+
     return main
 
 
