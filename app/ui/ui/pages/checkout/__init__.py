0284f15d7fd85d5d8da53b623955ecec0e55ef39
Indra Harijono <iharijono@gmail.com>
2023-11-28 16:57:17 -0800

    Added page for checkout

---
+++ app/ui/ui/pages/checkout/__init__.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+import solara
+
+from ... import data
+from ...components.checkout import Overview
+from ... import models
+from .. import user
+
+from sqlalchemy.orm import Session
+
+@solara.component
+def Page(name: Optional[str] = None, page: int = 0, page_size=100):
+    with solara.Column() as main:
+        solara.Title("ebreakfast » Checkout")
+        Overview()
+    return main
