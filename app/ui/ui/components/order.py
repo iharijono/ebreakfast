ce68100107568efd646c35eadeaf254cf362f28b
Indra Harijono <iharijono@gmail.com>
2023-11-30 10:02:01 -0800

    added button on order

--- app/ui/ui/components/order.py
+++ app/ui/ui/components/order.py
@@ -21,12 +21,25 @@ order_submitted = solara.reactive(False)
 
 @solara.component
 def MenuCard(name, menu):
+    quantity, set_quantity = solara.use_state(0)
+    def increment():
+        set_quantity(quantity + 1)
+    def decrement():
+        if quantity > 0:
+            set_quantity(quantity - 1)
+            
     with rv.Card(max_width="400px") as main:
         # with solara.Link(f"/menu/{name}"):
         rv.Img(height="250", src=menu.image_url)
         rv.CardTitle(children=[menu.price])
         with rv.CardText():
             solara.Markdown(menu.markdown)
+        with solara.Row(justify="space-around", margin=10):
+            solara.Button("add", color="green", icon_name="add", on_click=increment)
+            solara.Button(f"{quantity}", color="yellow")
+            solara.Button("min", color="red", icon_name="remove", on_click=decrement)
+            
+            
     oitem = OrderItem(name=name, quantity=2, price=menu.price)
     order.value.menuitems.append(oitem)
     return main
